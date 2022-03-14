64c1292a9aff514fa6e6db89d4d49687b88bc908
bcrisologo <jblaise0912@gmail.com>
2022-03-14 16:52:16 -0700

    Cleaned up setup

--- python/find-document.py
+++ python/find-document.py
@@ -2,28 +2,33 @@
 
 import os
 import requests
+import json
 
 data_api_endpoint = os.environ.get('data_api_url')
 data_api_name = os.environ.get('data_api_name')
 data_api_key = os.environ.get('data_api_key')
 
-r = requests.get(data_api_endpoint)
-print(r.status_code)
-
 # Action
-data_api_endpoint_crud = data_api_endpoint + '/action/'
-findOne = data_api_endpoint_crud + 'findOne'
+findOne = data_api_endpoint + '/action/findOne'
 
-print(findOne)
-
-pload = {
-    "url": findOne,
-    "headers": {
-        "Content-Type": "application/json",
-        "Access-Control-Request-Headers": "*",
-        "api-key": data_api_key
-    },
-    "data": {
-        
-    }
+pheader = {
+    "api-key": 'data_api_key',
+    "Content-Type": "application/json",
+    "Access-Control-Request-Headers": "*"
 }
+
+pload = json.dumps({
+    "data-raw": {
+        "dataSource": "shared-tier-tf",
+        "database": "sample_training",
+        "collection": "grades",
+        "filter": {
+            "student_id": 4
+        }
+    }
+})
+
+# HTTP Requests
+r = requests.request("POST", findOne, headers=pheader, data=pload)
+print(r.json())
+print(r.status_code)
